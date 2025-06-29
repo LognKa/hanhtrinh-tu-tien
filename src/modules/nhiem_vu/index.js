3236b909635ed59ed46969b51610be5cee7924b3
LognKa <hoangtiep.hqt@gmail.com>
2025-06-29 18:30:45 +0700

    Create index.js
    export const nhiemVu = [
      {
        id: 1,
        name: "Tiêu diệt 5 yêu quái",
        reward: { exp: 50, item: "Linh Thạch" },
        completed: false
      },
      {
        id: 2,
        name: "Thu thập 3 dược thảo",
        reward: { exp: 30, item: "Đan Dược" },
        completed: false
      }
    ];

---
+++ src/modules/nhiem_vu/index.js
@@ -0,0 +1,14 @@
+export const nhiemVu = [
+  {
+    id: 1,
+    name: "Tiêu diệt 5 yêu quái",
+    reward: { exp: 50, item: "Linh Thạch" },
+    completed: false
+  },
+  {
+    id: 2,
+    name: "Thu thập 3 dược thảo",
+    reward: { exp: 30, item: "Đan Dược" },
+    completed: false
+  }
+];
