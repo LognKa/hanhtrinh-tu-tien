82e01cd983104afe60fa2d7973a0433aa4428e3d
LognKa <hoangtiep.hqt@gmail.com>
2025-06-29 18:29:08 +0700

    Create index.js
    export const kyNang = {
      'Hỏa Cầu': {
        type: 'fire',
        damage: 30,
        manaCost: 10,
        description: 'Phóng ra cầu lửa gây sát thương.',
      },
      'Băng Tiễn': {
        type: 'ice',
        damage: 25,
        manaCost: 8,
        description: 'Phóng tiễn băng làm chậm kẻ địch.',
      },
    };

---
+++ src/modules/ky_nang/index.js
@@ -0,0 +1,14 @@
+export const kyNang = {
+  'Hỏa Cầu': {
+    type: 'fire',
+    damage: 30,
+    manaCost: 10,
+    description: 'Phóng ra cầu lửa gây sát thương.',
+  },
+  'Băng Tiễn': {
+    type: 'ice',
+    damage: 25,
+    manaCost: 8,
+    description: 'Phóng tiễn băng làm chậm kẻ địch.',
+  },
+};
