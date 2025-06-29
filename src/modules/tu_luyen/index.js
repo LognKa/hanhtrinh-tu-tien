d53846d3a19fe0037f4aa5a8f2a92187e9b1bcef
LognKa <hoangtiep.hqt@gmail.com>
2025-06-29 18:21:37 +0700

    Delete src/modules/tu_luyen directory

--- src/modules/tu_luyen/index.js
+++
@@ -1,6 +0,0 @@
-export function tuLuyen(character) {
-  if (character.energy < 10) return "Không đủ linh lực để tu luyện.";
-  character.exp += 10;
-  character.energy -= 10;
-  return "Tu luyện thành công, nhận 10 kinh nghiệm.";
-}
